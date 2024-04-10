c0f2a874a6178be491e8ef9f4fb0a8fbae2c31fd
Lavanya Mishra <lavanyamishra1996@gmail.com>
2024-04-10 11:57:02 +0530

    feat: add vuetify.

--- app/src/main.js
+++ app/src/main.js
@@ -5,10 +5,19 @@ import App from './App.vue'
 import router from './router/routes.js'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 const pinia = createPinia()
+const vuetify = createVuetify({
+    components,
+    directives,
+})
 const app = createApp(App)
 
+app.use(vuetify)
 app.use(VueAxios, axios)
 app.provide('axios', app.config.globalProperties.axios)
 app.use(router)
